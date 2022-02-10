0fed86a6e06fa3a5d13be154ec2069a2ac05f152
swanandsb <62928785+swanandsb@users.noreply.github.com>
2022-02-10 14:26:09 -0800

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
-const express = require("express");
-const app = express();
-const ServerFunction = express.static("public");
-app.use("/", ServerFucntion);
+const express = require("express")
+
+const app = express()
+
+// this lets express serve static files from public directory
+const fileServerMiddleware = express.static("public")
+
+app.use("/", fileServerMiddleware)
+
 app.listen(3000, () => {
-  console.log("Server started at Port 3000");
-});
+  console.log("Server started at port 3000");
+})
